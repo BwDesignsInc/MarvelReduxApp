c89f595bd213232cea9d7d24eeff353ff8811801
BwDesignsInc <garypboyd@rogers.com>
2020-02-20 22:49:47 -0500

    Feature/marvel api (#7)
    * fix details

--- src/features/Characters/actions.js
+++ src/features/Characters/actions.js
@@ -1,15 +1,19 @@
 import { mapKeys } from "lodash";
 import http, { config } from "../../api";
-import { CHARACTERS_REQUEST, CHARACTERS_SUCCESS, CHARACTERS_FAILURE } from "./types";
-const apiKey = "680e11e6ae10cd6d8b0dbedc8514a138";
-const makeCharacterUrl = ({ name }) => `/characters?name=${name}&apikey=${apiKey}`;
-const URI = "/v1/public/characters";
+import {
+  CHARACTERS_REQUEST,
+  CHARACTERS_SUCCESS,
+  CHARACTERS_FAILURE
+} from "./types";
 
+const URI = "/v1/public/characters";
 export class marvelCharacterAPI {
   static getCharacters(httpOptions = {}) {
     const defaultOptions = { page: 1, count: 20, name: "", nameStartsWith: "" };
     const options = { ...defaultOptions, ...httpOptions };
-    const currentOffset = options.page === 1 ? 0 : options.count * (options.page - 1);
+    const currentOffset =
+      options.page === 1 ? 0 : options.count * (options.page - 1);
+
     let params = `?apikey=${config.publicKey}&limit=${options.count}&offset=${currentOffset}`;
     if (options.name) {
       params = params.concat(`&name=${options.name}`);
@@ -20,6 +24,7 @@ export class marvelCharacterAPI {
     const url = `${config.baseUrl}${URI}${params}`;
     return http.get(url);
   }
+
 
   static getComicsByCharacter(characterId, offset = 0) {
     const URI = `/v1/public/characters/${characterId}/comics`;
